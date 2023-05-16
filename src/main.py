32ebcd230812c83ad7d13d637064c25709caea29
Reventlow <gorm@reventlow.com>
2023-05-16 09:50:17 +0200

    initial release

--- src/main.py
+++ src/main.py
@@ -25,41 +25,44 @@ def change_password_in_studsys(driver: webdriver, search_user: str) -> tuple[str
 
 
 def main():
-    # Clear terminal
-    tools.clear()
+    while True:
+        # Clear terminal
+        tools.clear()
 
-    # Start selenium webdriver headless
-    driver = selenium_tools.get_webdriver()
+        # Start selenium webdriver headless
+        driver = selenium_tools.get_webdriver()
 
-    # Get username or cpr-number from user
-    search_user = input("Enter username or cpr-number: ")
+        # Get username or cpr-number from user
+        search_user = input("Enter username or cpr-number: ")
 
-    # Change to password on student in Studsys
-    sms_msg, this_mobile = change_password_in_studsys(driver, search_user)
-
-
-    # Close selenium webdriver
-    driver.close()
-
-    # Send sms to user
-    print(f"Is this the correct mobile number: {this_mobile}")
-    send_sms = input("Send sms to user? (y/n): ")
-    if send_sms == "y":
-        unord_sms.send_sms(sms_msg, this_mobile)
-    else:
-        this_mobile = input("Enter new mobile number: ")
-        unord_sms.send_sms(sms_msg, this_mobile)
-    print(f"SMS sent to {this_mobile}")
-
-    # Print sms message
-    print(sms_msg)
-
-    # Exit program
-    print("Exiting...")
-    pyperclip.copy(sms_msg)
-    sys.exit()
+        # Change to password on student in Studsys
+        sms_msg, this_mobile = change_password_in_studsys(driver, search_user)
 
 
+        # Close selenium webdriver
+        driver.close()
+
+        # Send sms to user
+        sms_response = {}
+        print(f"Is this the correct mobile number: {this_mobile}")
+        send_sms = input("Send sms to user? (y/n): ")
+        if send_sms == "y":
+            this_mobile = this_mobile.replace(" ", "").replace("+45", "")
+            sms_response = unord_sms.send_sms(this_mobile, sms_msg)
+        else:
+            this_mobile = input("Enter new mobile number: ")
+            sms_response = unord_sms.send_sms(this_mobile, sms_msg)
+        print(f"SMS sent to {this_mobile}")
+
+        # Print sms message
+        print(sms_msg)
+
+        # Copy sms message to clipboard
+        pyperclip.copy(sms_msg)
+
+        # Wait for user to press enter
+        print(f"Response for sms.dk: {sms_response}")
+        input("Press enter to to continue...")
 
 
 if __name__ == '__main__':
